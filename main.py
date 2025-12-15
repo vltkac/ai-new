12731784224658d9c8f78c753ff9d0135fb82e37
vltkac <vladtkachuk135@gmail.com>
2025-12-15 19:33:49 +0100

    pr committed_

--- main.py
+++ main.py
@@ -1,71 +1,73 @@
-# LLM
-# Large language model
-
-# загрузка API ключа с ключа .env как переменную среды
-
-# import os
-# import dotenv
-#
-#
-# dotenv.load_dotenv()
-# api_key = os.getenv('GEMINI_API_KEY')
-#
-# import langchain
-# from langchain_google_genai import GoogleGenerativeAI
-#
-#
-# llm = GoogleGenerativeAI(
-#     model='gemini-2.5-flash-lite',
-#     api_key=api_key
-# )
-#
-# response = llm.invoke('Hello, what are your salary expectations?')
-# print(response)
-
-
 # Завдання 1
-# Підключіть модель LLM за допомогою свого API key.
-# Попросіть модель згенерувати:
-# ● відповідь на питання у вигляді одного
-# слова(наприклад яка столиця Франції?)
-# ● код python
-# ● коротку історію
-# Підберіть параметри креативності та довжини
+# Напишіть модель для рекомендації книг з двох ланцюгів
+#  Перший ланцюг отримує назву книги та визначає її жанр
+#  Другий отримує назву книги, жанр та повертає список
+# схожих книг(того ж самого жанру та іншого)
 
 import os
 import dotenv
+from typing import List
+from pydantic import BaseModel, Field
 from langchain_google_genai import GoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser
 
 
 dotenv.load_dotenv()
-
-API_KEY = os.getenv('GEMINI_API_KEY')
+API_KEY = os.getenv(GEMINI_API_KEY)
 
 llm = GoogleGenerativeAI(
-    model='gemini-2.5-flash-lite',
+    model='gemini-2.5-flash',
+    api_key=API_KEY,
     temperature=0
 )
 
-user_input = input('Your question: ')
-# command1 = 'дай ответ одним словом. если ответ два и больше слова, то давай полный ответ. '
-# command_py = 'write response of the python code (only): '
-command_story = 'write story within 4 sentences. be creative and fun. '
+class BookInfo(BaseModel)
+    genre str = Field(description='жанр книги')
 
-response = llm.invoke(command_story + user_input)
-print(response)
+parser = PydanticOutputParser(pydantic_object=BookInfo)
 
+instructions = parser.get_format_instructions()
 
+prompt = PromptTemplate.from_template("""Ти - бібліотекар. Твоє завдання полягає в тому, щоб визначати жанри книг.
+### ІСТРУКЦІЇ
+{instructions}
 
+### НАЗВА КНИГИ
+{book_title}"""
+, partial_variables={'instructions': instructions})
 
+chain1 = prompt | llm | parser
 
+user_book = input('Введіть назву книги ')
 
+response = chain1.invoke({
+    'book_title': user_book
+})
 
+class UpgradedBookInfo(BaseModel):
+    books: List[str] = Field(description='список схожих книг')
 
+parser = PydanticOutputParser(pydantic_object=UpgradedBookInfo)
 
+instructions = parser.get_format_instructions()
 
+prompt = PromptTemplate.from_template("""Ти - бібліотекар. Твоє завдання полягає в тому, щоб радити подібні книги на основі назви книги та жанру.
+### ІСТРУКЦІЇ
+{instructions}
 
+### НАЗВА КНИГИ
+{book_title}
 
+### ЖАНР
+{book_genre}""", partial_variables={'instructions': instructions})
 
+chain1 = prompt | llm | parser
 
+full_response = chain1.invoke({
+    'book_title': user_book,
+    'book_genre': response.genre
+})
 
-
+for book in full_response.books:
+    print(book)
